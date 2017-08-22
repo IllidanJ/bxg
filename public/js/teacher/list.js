a05b3c69d1d545e0221124be8ea159fdd33a5013
IllidanJ <393503475@qq.com>
2017-08-22 11:56:24 +0800

    修改了注销按钮bug

--- public/js/teacher/list.js
+++ public/js/teacher/list.js
@@ -10,12 +10,20 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
       if (info.code == 200) {
         var html = template('teacher_list_temp', info);
         $('#teacher_list').html(html);
+        $('.handle').each(function () {
+          if ($(this).parent().data('status') == 0) {
+            $(this).addClass('btn-warning').html('注 销');
+          }else{
+            $(this).addClass('btn-success').html('启 用');
+          }
+        })
+
       }
 
     }
-  })
+  });
   //查看按钮
-  $("#teacher_list").on('click', '#chakan', function () {
+  $("#teacher_list").on('click', '.preview', function () {
     var tc_id = $($(this).parent()[0]).data('id');
     console.log(tc_id);
     $(".modal").modal('show');
@@ -33,7 +41,7 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
     });
   });
   //启用注销按钮
-  $("#teacher_list").on('click', '#zhuxiao', function () {
+  $("#teacher_list").on('click', '.handle', function () {
     var tc_status = $(this).parent().data('status');
     var tc_id = $(this).parent().data('id');
     var $self = $(this);
@@ -42,17 +50,17 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
       url: '/api/teacher/handle',
       data: {
         tc_id: tc_id,
-        tc_status:tc_status
+        tc_status: tc_status
       },
-      success:function (info) {
-        if(info.code==200) {
+      success: function (info) {
+        if (info.code == 200) {
           tc_status = info.result.tc_status;
         }
       },
-      complete:function () {
-        if(tc_status==1) {
+      complete: function () {
+        if (tc_status == 1) {
           $self.html('启 用').removeClass('btn-warning').addClass('btn-success').parent().data('status', tc_status);
-        }else{
+        } else {
           $self.html('注 销').addClass('btn-warning').removeClass('btn-success').parent().data('status', tc_status);
         }
       }
